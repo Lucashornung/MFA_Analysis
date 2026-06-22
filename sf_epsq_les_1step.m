761e0f834f3f5660ff1bc2514be0815487b9c0bd
Lucas Hornung <lucashornung42@gmail.com>
2026-06-22 15:31:07 -0700

    Update output filename path to use varargin

--- sf_epsq_les_1step.m
+++ sf_epsq_les_1step.m
@@ -77,7 +77,7 @@ slopet = slope(:, ti);
 
 %% Set output filename
 tstr   = sprintf('_%05i', ti);
-outnmt = fullfile(fd, strcat(outnm, tstr, '.mat'));
+outnmt = fullfile(varargin{2}, strcat(outnm, tstr, '.mat'));
 
 %% Short-circuit if no levels meet threshold
 top = find(frac > thresh, 1, 'last');
